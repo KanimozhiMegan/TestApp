dbef88798ab37d74f86030bfde715f6bd5fd6619
Periyanayaki <pv0076416@techmahindra.com>
2014-03-13 19:02:58 +0530

    Adding my project to remote location

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '108783da736fb9e555b10ef00b130e3e6210b519971dabf570b9c3774c0171144ecd30c33430902379b286178b414a4afb51ffd4b5a6b3460db75e422a99f1f0'
